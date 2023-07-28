bd6e2deb0c882b17011667bda9fbf3fa364bb60b
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-07-28 16:13:56 -0300

    Create first report files

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,106 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('solidity-audit-report-generator.generateAuditReport', () => {
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-
-        if (!workspaceFolders) {
-            vscode.window.showInformationMessage('No workspace is opened.');
-            return;
-        }
-
-        const auditComments: string[] = [];
-
-        workspaceFolders.forEach((folder) => {
-            const folderPath = folder.uri.fsPath;
-            findSolidityFiles(folderPath, auditComments);
-        });
-
-        const auditCommentsFilePath = path.join(workspaceFolders[0].uri.fsPath, 'audit_comments.txt');
-        fs.writeFileSync(auditCommentsFilePath, auditComments.join('\n'), 'utf8');
-        vscode.window.showInformationMessage('Audit comments saved to audit_comments.txt.');
-    });
-
-    context.subscriptions.push(disposable);
+interface Finding {
+  id: string;
+  file: string;
+  line: string;
+  description: string;
+  recommendation?: string;
+  references?: string;
 }
 
-function findSolidityFiles(dirPath: string, auditComments: string[]) {
-    fs.readdirSync(dirPath).forEach(file => {
-        const filePath = path.join(dirPath, file);
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "solidity-audit-report-generator.generateAuditReport",
+    () => {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
 
-        if (fs.statSync(filePath).isDirectory()) {
-            findSolidityFiles(filePath, auditComments);
-        } else if (path.extname(file) === '.sol') {
-            const fileContent = fs.readFileSync(filePath, 'utf8');
-            const lines = fileContent.split('\n');
-            lines.forEach((line, i) => {
-                if (line.trim().startsWith('// @audit')) {
-                    auditComments.push(`File: ${file}, Line: ${i + 1}, Comment: ${line.trim()}`);
-                }
-            });
+      if (!workspaceFolders) {
+        vscode.window.showInformationMessage("No workspace is opened.");
+        return;
+      }
+
+      const dir = workspaceFolders[0].uri.fsPath;
+      const findingsDir = path.join(dir, "findings");
+      if (!fs.existsSync(findingsDir)) {
+        fs.mkdirSync(findingsDir);
+      }
+
+      const findings: Finding[] = [];
+
+      workspaceFolders.forEach((folder) => {
+        const folderPath = folder.uri.fsPath;
+        findAuditIssues(folderPath, findings);
+      });
+
+      findings.map((finding) => {
+        const auditCommentsFilePath = path.join(
+          findingsDir,
+          `${finding.id}.xml`
+        );
+        vscode.window.showInformationMessage(`Finding ${finding.id} saved`);
+        console.log(finding);
+        const body = [
+          `<file>${finding.file}</file>`,
+          `<line>${finding.line}</line>`,
+          `<description>${finding.description}</description>`,
+          finding.recommendation
+            ? `<recommendation>${finding.recommendation}</recommendation>`
+            : undefined,
+          finding.references
+            ? `<references>${finding.references}</references>`
+            : undefined,
+        ]
+          .filter((x) => x)
+          .join("\n");
+        fs.writeFileSync(auditCommentsFilePath, body, "utf8");
+      });
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+function findAuditIssues(dirPath: string, findings: Finding[]): void {
+  const AUDIT_ISSUE = "// @audit-issue ";
+
+  fs.readdirSync(dirPath).forEach((file) => {
+    const filePath = path.join(dirPath, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      findAuditIssues(filePath, findings);
+    } else if (path.extname(file) === ".sol") {
+      const fileContent = fs.readFileSync(filePath, "utf8");
+      const lines = fileContent.split("\n");
+      lines.forEach((line, i) => {
+        if (line.trim().startsWith(AUDIT_ISSUE)) {
+          const text = line.trim().replace(AUDIT_ISSUE, "");
+          const match = text.match(
+            /(.*)[ @recommendation (.*)[ @references (.*)]?]?/
+          );
+          if (!match) {
+            console.log(text);
+            return;
+          }
+          const [, idAndDescription, recommendation, references] =
+            match as string[];
+          const [id, ...desc] = idAndDescription.split(" ");
+          const description = desc.join(" ");
+          const lineNumber = (i + 1 + 1).toString();
+          findings.push({
+            file,
+            line: lineNumber,
+            id,
+            description,
+            recommendation,
+            references,
+          });
         }
-    });
+      });
+    }
+  });
 }
 
 export function deactivate() {}
